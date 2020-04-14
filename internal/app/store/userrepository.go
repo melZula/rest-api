e40f761257152a9205fd8b0ffa31f37f6e2eac08
melZula <zula0747@gmail.com>
2020-04-14 20:44:49 +0300

    findByEmail func, tests for funcs

--- internal/app/store/userrepository.go
+++ internal/app/store/userrepository.go
@@ -8,11 +8,28 @@ type UserRepository struct {
 }
 
 // Create ...
-func (r *UserRepository) Create(u model.User) (model.User, error) {
-	return nil, nil
+func (r *UserRepository) Create(u *model.User) (*model.User, error) {
+	if err := r.store.db.QueryRow(
+		"INSERT INTO users (email, encrypted_password) VALUES ($1, $2) RETURNING id",
+		u.Email,
+		u.EncryptedPassword,
+	).Scan(&u.ID); err != nil {
+		return nil, err
+	}
+	return u, nil
 }
 
 // FindByEmail ...
-func (r *UserRepository) FindByEmail(email string) {
-	return nil, nil
+func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
+	u := &model.User{}
+	if err := r.store.db.QueryRow("SELECT id, email, encrypted_password FROM users WHERE email = $1",
+		email,
+	).Scan(
+		&u.ID,
+		&u.Email,
+		&u.EncryptedPassword,
+	); err != nil {
+		return nil, err
+	}
+	return u, nil
 }
