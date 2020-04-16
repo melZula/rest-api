13f1cb48a10ca06a3de6dc0c25c9c8254e501fde
melZula <zula0747@gmail.com>
2020-04-16 03:11:47 +0300

    validations+table testing

--- internal/app/model/user_test.go
+++ internal/app/model/user_test.go
@@ -7,6 +7,78 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestUser_Validate(t *testing.T) {
+	testCases := []struct {
+		name    string
+		u       func() *model.User
+		isValid bool
+	}{
+		{
+			name: "valid",
+			u: func() *model.User {
+				return model.TestUser(t)
+			},
+			isValid: true,
+		},
+		{
+			name: "with_encrypted_password",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Password = ""
+				u.EncryptedPassword = "3rewre3r77ew89ewf64"
+				return u
+			},
+			isValid: true,
+		},
+		{
+			name: "empty_email",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Email = ""
+				return u
+			},
+			isValid: false,
+		},
+		{
+			name: "invalid_email",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Email = "invalid.sd@"
+				return u
+			},
+			isValid: false,
+		},
+		{
+			name: "short_password",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Password = "short"
+				return u
+			},
+			isValid: false,
+		},
+		{
+			name: "empty_password",
+			u: func() *model.User {
+				u := model.TestUser(t)
+				u.Password = ""
+				return u
+			},
+			isValid: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.isValid {
+				assert.NoError(t, tc.u().Validate())
+			} else {
+				assert.Error(t, tc.u().Validate())
+			}
+		})
+	}
+}
+
 func TestUser_BeforeCreate(t *testing.T) {
 	u := model.TestUser(t)
 	assert.NoError(t, u.BeforeCreate())
